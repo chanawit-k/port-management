1dee854f66af5ca1ff71d7c9c2fdfaba7a252269
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-25 17:04:10 +0700

    - dummy data

---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,90 @@
+export const dummyData = [
+  {
+    id: 1,
+    portName: 'fund Port 1',
+    topHoding: [
+      { name: 'USA', percent: '50%' },
+      { name: 'China', percent: '50%' },
+      { name: 'Oil', percent: '20%' },
+      { name: 'Gold', percent: '10%' },
+      { name: 'Thai', percent: '5%' },
+    ],
+  },
+  {
+    id: 2,
+    portName: 'fund Port 2',
+    topHoding: [
+      { name: 'USA', percent: '40%' },
+      { name: 'China', percent: '30%' },
+      { name: 'Oil', percent: '25%' },
+      { name: 'Gold', percent: '15%' },
+      { name: 'Thai', percent: '10%' },
+    ],
+  },
+  {
+    id: 3,
+    portName: 'fund Port 3',
+    topHoding: [
+      { name: 'USA', percent: '45%' },
+      { name: 'China', percent: '35%' },
+      { name: 'Oil', percent: '15%' },
+      { name: 'Gold', percent: '10%' },
+      { name: 'Thai', percent: '5%' },
+    ],
+  },
+  {
+    id: 4,
+    portName: 'fund Port 4',
+    topHoding: [
+      { name: 'USA', percent: '60%' },
+      { name: 'China', percent: '20%' },
+      { name: 'Oil', percent: '10%' },
+      { name: 'Gold', percent: '5%' },
+      { name: 'Thai', percent: '5%' },
+    ],
+  },
+  {
+    id: 5,
+    portName: 'fund Port 5',
+    topHoding: [
+      { name: 'USA', percent: '55%' },
+      { name: 'China', percent: '25%' },
+      { name: 'Oil', percent: '10%' },
+      { name: 'Gold', percent: '5%' },
+      { name: 'Thai', percent: '5%' },
+    ],
+  },
+  {
+    id: 6,
+    portName: 'fund Port 6',
+    topHoding: [
+      { name: 'USA', percent: '45%' },
+      { name: 'China', percent: '35%' },
+      { name: 'Oil', percent: '15%' },
+      { name: 'Gold', percent: '3%' },
+      { name: 'Thai', percent: '2%' },
+    ],
+  },
+  {
+    id: 7,
+    portName: 'fund Port 7',
+    topHoding: [
+      { name: 'USA', percent: '50%' },
+      { name: 'China', percent: '25%' },
+      { name: 'Oil', percent: '10%' },
+      { name: 'Gold', percent: '10%' },
+      { name: 'Thai', percent: '5%' },
+    ],
+  },
+  {
+    id: 8,
+    portName: 'fund Port 8',
+    topHoding: [
+      { name: 'USA', percent: '70%' },
+      { name: 'China', percent: '20%' },
+      { name: 'Oil', percent: '5%' },
+      { name: 'Gold', percent: '3%' },
+      { name: 'Thai', percent: '2%' },
+    ],
+  },
+]
