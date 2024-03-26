a02b166a845e18fe6838498defdab57ce2973e80
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-26 16:49:26 +0700

    - update model

--- models/PortModels.js
+++ models/PortModels.js
@@ -6,7 +6,12 @@ const portSchema = mongoose.Schema({
   updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   updatedOn: { type: Date, default: Date.now },
   transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }],
-  stocks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Stock' }],
+  stocks: [
+    {
+      stock: { type: mongoose.Schema.Types.ObjectId, ref: 'Stock' }, // Reference to Stock model
+      value: { type: Number, default: 0 }, // Value associated with the stock in this port
+    },
+  ],
 })
 
 export default mongoose.model('Port', portSchema)
--- models/StockModels.js
+++ models/StockModels.js
@@ -1,6 +1,5 @@
 const stockSchema = mongoose.Schema({
   name: { type: String, required: true }, // Name of the stock
-  value: { type: Number, default: 0 }, // Value associated with the stock
 })
 
 export default mongoose.model('Stock', stockSchema)
