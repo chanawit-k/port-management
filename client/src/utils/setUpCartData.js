d74ede7cd5fe3fbc637a8b56fc07cadd4272540a
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-27 15:37:35 +0700

    - chart data

---
+++ client/src/utils/setUpCartData.js
@@ -0,0 +1,25 @@
+export const setUpCartData = (stocks) => {
+  return {
+    labels: stocks.map((stock) => stock.stock.name),
+    datasets: [
+      {
+        data: stocks.map((stock) => stock.value),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+}
