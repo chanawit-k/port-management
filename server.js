0b9951ef480003d8831d33aa5777eed892783388
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-27 11:30:47 +0700

    - add json

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-import express from 'express'
+import 'express-async-errors'
 import * as dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 dotenv.config()
+import connectDB from './db/connect.js'
+import express from 'express'
+import portRouter from './routes/portRouter.js'
+
+// public
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+// middleware
+import notFound from './middleware/notfoundMiddleware.js'
+
 const app = express()
-import { connectDB } from './db/connect.cjs'
+app.use(express.json())
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
+app.use(cookieParser())
+
+// routers
+app.use('/api/v1/port', portRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+// middleware
+app.use(notFound)
 
 const port = process.env.PORT || 5100
 const start = async () => {
