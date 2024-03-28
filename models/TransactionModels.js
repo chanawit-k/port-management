a7fb51fc3be2e1fa484bdcec2081ac01ef5a9d51
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-28 10:52:34 +0700

    - add model transaction

--- models/TransactionModels.js
+++ models/TransactionModels.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
+
 const transactionSchema = mongoose.Schema({
   description: String,
-  amount: Number,
-  date: { type: Date, default: Date.now },
+  dateIn: { type: Date },
+  dateOut: { type: Date },
+  priceIn: Number,
+  priceOut: Number,
+  unitIn: Number,
+  unitOut: Number,
+  realizedGain: Number,
+  realizedGainPercent: Number,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   port: { type: mongoose.Schema.Types.ObjectId, ref: 'Port' },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdOn: { type: Date, default: Date.now },
+  updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  updatedOn: { type: Date, default: Date.now },
 })
 
 export default mongoose.model('Transaction', transactionSchema)
